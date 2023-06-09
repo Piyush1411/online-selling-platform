dcd30119df76806ec84de7cd0d0b15c81b69ef0d
Piyush1411 <piyushggupta1411@gmail.com>
2023-06-09 06:01:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('../middlewares/auth');
+
+// Seller dashboard
+router.get('/dashboard', verifyToken, (req, res) => {
+  res.sendFile('dashboard.html', { root: 'public' });
+});
+
+module.exports = router;
+
+// Seller dashboard
+router.get('/dashboard/:sellerId', (req, res) => {
+  res.sendFile('dashboard.html', { root: 'public' });
+});
+
+module.exports = router;
